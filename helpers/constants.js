d3de49ea516f6601aeed110f81352d1e91d1dcd9
kvyat8 <102686018+kvyat8@users.noreply.github.com>
2024-12-19 18:39:40 +0300

    Update constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,12 +1,12 @@
 export const capperUser = {
-  email: 'autotest-capper@stvteam.ru',
+  email: 'autotestcapper1111@yandex.ru',
   password: 'password',
-  firstName: 'Автотест',
-  secondName: 'Интерфейс',
+  firstName: 'Авто',
+  secondName: 'Матон',
   slug: 'avtotest-interfeis'
 }
 
-export const apiUrl = 'https://v2dev.stavka.tv/api/v2'
+export const apiUrl = 'https://stavka.tv/api/v2'
 export const sportsArray = ['soccer', 'ice-hockey', 'tennis', 'basketball']
 
 // email: "torterulmo@gufum.com",
