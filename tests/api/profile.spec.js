b83e7424c6169e5d8a06449615c19104b073c304
Kvyat <kvyvladimir@example.com>
2024-12-20 11:30:05 +0300

    tg to env

--- tests/api/profile.spec.js
+++ tests/api/profile.spec.js
@@ -3,6 +3,7 @@ import { test, expect, step } from '../../helpers/fixtures'
 test.describe('API Профиль', () => {
   test('Корректная авторизация профиля POST /auth/login', { tag: ['@API'] }, async ({ api }) => {
     let token = await api.profileService.getAuthToken()
+    console.log(process.env)
     await step(`API. Валидация токена`, async () => {
       expect(typeof token === 'string').toBe(true)
     })
